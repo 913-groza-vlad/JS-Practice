78c4d133ed1f44c55ce219ba1b2dd3999789cd3e
Vlad Groza <vlad.andrei.groza@stud.ubbcluj.ro>
2023-08-24 16:50:11 +0300

    implemented accordion component

--- scripts/sidebar-script.js
+++ scripts/sidebar-script.js
@@ -6,7 +6,23 @@ hamburger.addEventListener("click", () => {
   menu.classList.toggle("active");
 });
 
-document.querySelectorAll(".nav-link").forEach((n) => n.addEventListener("click", () => {
-  hamburger.classList.remove("active");
-  menu.classList.remove("active");
-}));
+const useful = document.querySelector(".nav-item.useful");
+const accordion = document.querySelector(".accordion");
+
+useful.addEventListener("click", () => {
+    useful.classList.toggle("sub-active");
+    accordion.classList.toggle("sub-active");
+  });
+
+const displaySubmenu = (category, submenu) => {
+  const categoryElements = document.getElementsByClassName(`nav-item ${category}`);
+  const submenuElements = document.getElementsByClassName(submenu);
+
+  categoryElements[0].addEventListener("click", () => {
+      categoryElements[0].classList.toggle("sub-active2");
+      submenuElements[0].classList.toggle("sub-active2");
+    });
+}
+
+displaySubmenu('category2', 'submenu-cat2');
+displaySubmenu('category4', 'submenu-cat4');
