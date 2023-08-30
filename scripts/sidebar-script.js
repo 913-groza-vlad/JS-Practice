03aeff0e55702043044762e9c094eba468ae81bf
Vlad Groza <vlad.andrei.groza@stud.ubbcluj.ro>
2023-08-30 10:45:30 +0300

    update sidebar menu

--- scripts/sidebar-script.js
+++ scripts/sidebar-script.js
@@ -1,4 +1,65 @@
 (() => {
+  const displaySubmenu = (category) => {
+    const categoryElements = document.getElementsByClassName(`nav-item ${category}`);  
+    const submenuElems = categoryElements[0].nextElementSibling;
+      
+    categoryElements[0].addEventListener("click", () => {
+      categoryElements[0].classList.toggle("sub-active2");
+      submenuElems.classList.toggle("sub-active2");
+        
+      const getAllSubActive2 = document.querySelectorAll(".sub-active2");
+      getAllSubActive2.forEach((element) => {
+        if (element.classList.contains("sub-active2") && element !== submenuElems) {
+          element.classList.remove("sub-active2");
+        }
+      });
+    });
+  };
+  
+  
+  const addSubmenusToMenu = (parrentEl) => {
+    // ToDo: 1. remove elemnts with classes accordion and submenu from HTML and generate them here
+    // 2. Append the generated accordion element to the parrentEl
+    const accordion = document.createElement("div");
+    accordion.classList.add("accordion");
+    const submenu = document.createElement("ul");
+    submenu.classList.add(".submenu");
+    accordion.appendChild(submenu);
+
+    const menu = document.querySelector(".menu");
+    menu.appendChild(accordion);
+
+    parrentEl.submenuData.forEach((itemData, index) => {
+      const listItem = document.createElement("li");
+      listItem.classList.add("nav-item");
+
+      const p = document.createElement("p");
+      p.textContent = itemData.name;
+      listItem.appendChild(p);
+
+      if (itemData.submenuItems) {
+        const arrowImg = document.createElement("img");
+        arrowImg.classList.add("arrow-symbol");
+        arrowImg.src = "assets/Icon ionic-ios-arrow-forward-3.svg";
+        arrowImg.alt = "down-arrow";
+        listItem.appendChild(arrowImg);
+
+        const subMenuDiv = document.createElement("div");
+        subMenuDiv.classList.add("submenu-category");
+        listItem.classList.add(`category-${index + 1}`);
+        itemData.submenuItems.forEach((submenuItem) => {
+          const subMenuItem = document.createElement("p");
+          subMenuItem.textContent = submenuItem;
+          subMenuDiv.appendChild(subMenuItem);
+        });
+        submenu.appendChild(listItem);
+        submenu.appendChild(subMenuDiv);
+
+        displaySubmenu(listItem.classList[1]);
+      } else submenu.appendChild(listItem);
+    });
+  };
+
   const usefulSubmenuData = [
     {name: "Category 1"},
     {name: "Category 2", submenuItems: ["Category 2.1", "Category 2.2"]},
@@ -6,9 +67,16 @@
     {name: "Category 4", submenuItems: ["Category 4.1", "Category 4.2"]},
     {name: "Category 5", submenuItems: ["Category 5.1", "Category 5.2", "Category 5.3"]},
   ];
+
+  const locatonsSubmenuData = [
+    {name: "Category 1"},
+    {name: "Category 2", submenuItems: ["Category 2.1"]},
+    {name: "Category 3", submenuItems: ["Category 3.1", "Category 3.2"]},
+  ];
+
   const menuData = [
     {icon: "assets/medici icon.svg", label: "Doctors"},
-    {icon: "assets/XMLID_2_.svg", label: "Locations"},
+    {icon: "assets/XMLID_2_.svg", label: "Locations", submenuData: locatonsSubmenuData},
     {icon: "assets/specializari.svg", label: "Specialisations"},
     {icon: "assets/XMLID_2192_.svg", label: "Prices"},
     {icon: "assets/help.svg", label: "Useful", submenuData: usefulSubmenuData},
@@ -34,69 +102,19 @@
 
     if (itemData.submenuData) {
       const arrowImg = document.createElement("img");
+      arrowImg.classList.add("arrow-symbol");
       arrowImg.src = arrowSrc;
       arrowImg.alt = "down-arrow";
-      arrowImg.style.marginLeft = "115px";
       listItem.classList.add("useful");
       listItem.appendChild(arrowImg);
-      addSubmenusToMenu(listItem);
+      menuList.appendChild(listItem);
+      addSubmenusToMenu(itemData, listItem);
     }
-
-    menuList.appendChild(listItem);
+    else
+      menuList.appendChild(listItem);
   });
 
-  const addSubmenusToMenu = (parrentEl) => {
-    // ToDo: 1. remove elemnts with classes accordion and submenu from HTML and generate them here
-    // 2. Append the generated accordion element to the parrentEl
-    const submenu = document.querySelector(".submenu");
-
-    submenuData.forEach((itemData, index) => {
-      const listItem = document.createElement("li");
-      listItem.classList.add("nav-item");
-
-      const p = document.createElement("p");
-      p.textContent = itemData.name;
-      listItem.appendChild(p);
-
-      if (itemData.submenuItems) {
-        const arrowImg = document.createElement("img");
-        arrowImg.src = "assets/Icon ionic-ios-arrow-forward-3.svg";
-        arrowImg.alt = "down-arrow";
-        listItem.appendChild(arrowImg);
-
-        const subMenuDiv = document.createElement("div");
-        subMenuDiv.classList.add("submenu-category");
-        listItem.classList.add(`category-${index + 1}`);
-        itemData.submenuItems.forEach((submenuItem) => {
-          const subMenuItem = document.createElement("p");
-          subMenuItem.textContent = submenuItem;
-          subMenuDiv.appendChild(subMenuItem);
-        });
-        submenu.appendChild(listItem);
-        submenu.appendChild(subMenuDiv);
-
-        displaySubmenu(listItem.classList[1]);
-      } else submenu.appendChild(listItem);
-    });
-  };
 })();
-
-const displaySubmenu = (category) => {
-  const categoryElements = document.getElementsByClassName(`nav-item ${category}`);
-  const submenuElems = categoryElements[0].nextElementSibling;
-
-  categoryElements[0].addEventListener("click", () => {
-    categoryElements[0].classList.toggle("sub-active2");
-    submenuElems.classList.toggle("sub-active2");
-
-    const getAllSubActive2 = document.querySelectorAll(".sub-active2");
-    getAllSubActive2.forEach((element) => {
-      if (element.classList.contains("sub-active2") && element !== submenuElems) {
-        element.classList.remove("sub-active2");
-      }
-    });
-  });
-};
 
 const hamburger = document.querySelector(".hamburger");
 const menu = document.querySelector(".navbar");
@@ -106,19 +124,21 @@ hamburger.addEventListener("click", () => {
   menu.classList.toggle("active");
 });
 
-const useful = document.querySelector(".nav-item.useful");
-const accordion = document.querySelector(".accordion");
+const usefuls = document.querySelectorAll(".nav-item.useful");
 
-useful.addEventListener("click", () => {
-  useful.classList.toggle("sub-active");
-  accordion.classList.toggle("sub-active");
-
-  const isAccordionActive = accordion.classList.contains("sub-active");
-  if (!isAccordionActive) {
-    const elementsWithClassSubActive = document.querySelectorAll(".sub-active2");
-
-    elementsWithClassSubActive.forEach((element) => {
-      element.classList.remove("sub-active2");
-    });
-  }
+usefuls.forEach((useful) => {
+  const accordion = useful.nextElementSibling;
+  useful.addEventListener("click", () => {
+    useful.classList.toggle("sub-active");
+    accordion.classList.toggle("sub-active");
+  
+    const isAccordionActive = accordion.classList.contains("sub-active");
+    if (!isAccordionActive) {
+      const elementsWithClassSubActive = document.querySelectorAll(".sub-active2");
+  
+      elementsWithClassSubActive.forEach((element) => {
+        element.classList.remove("sub-active2");
+      });
+    }
+  });
 });
